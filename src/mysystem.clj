b4cd8710d98ea0ae6bc41c4266726a69b4fb86d3
niquola <niquola@gmail.com>
2024-11-27 11:43:13 +0000

    init

---
+++ src/mysystem.clj
@@ -0,0 +1,46 @@
+(ns mysystem
+  (:require [system] [http] [pg]))
+
+(system/defmanifest
+  {:description "my system"})
+
+(defn get-index [context req]
+  {:status 200 :body "Hello"})
+
+(defn get-patient [context req]
+  {:status 200 :body (pg/execute! context ["select * from patients"])})
+
+(defn migrate [context]
+  (system/info context ::migrate "create tables etc..")
+  (pg/execute! context ["create table if not exists patients (id text primary key, resource jsonb)"])
+  (pg/execute! context ["truncate patients"])
+  (pg/execute! context ["insert into patients (id,resource) values ('pt-1', '{}')"])
+  )
+
+(system/defstart
+  [context config]
+
+  (http/register-endpoint context :get "/" #'get-index)
+  (http/register-endpoint context :get "/Patient" #'get-patient)
+
+  (migrate context)
+  {})
+
+
+(comment
+  (def context (system/start-system {:services ["http" "http.openapi" "pg" "mysystem"]
+                                     :http {:port 8882}
+                                     :pg {:host "localhost" :port 5400 :user "admin" :database "mysystem" :password "admin"}}))
+  (system/stop-system context)
+
+  (migrate context)
+  (get-index context {})
+
+  (http/request context {:path "/"})
+  (http/request context {:path "/api"})
+  (http/request context {:path "/Patient"})
+
+
+
+
+  )
