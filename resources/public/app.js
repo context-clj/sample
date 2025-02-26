66ae11f977454cf698419590207248002d53b939
niquola <niquola@gmail.com>
2025-02-26 22:52:19 +0000

    naive key management for select

--- resources/public/app.js
+++ resources/public/app.js
@@ -19,16 +19,66 @@ function inputWithPopup(id, pid) {
   inp.addEventListener('focus', () => {
     // console.log('open', inp, pop)
     pop.classList.toggle('hidden', false);
+    htmx.trigger(inp, 'open')
   });
 
   inp.addEventListener('blur', () => {
     setTimeout(()=>{
       // console.log('close', inp, pop)
       pop.classList.toggle('hidden', true);
+      // todo clear popup
     }, 100)
+  });
+
+  var currentIndex = 0;
+
+  function selectDiv(idx,divs) {
+    console.log('selection', idx);
+    htmx.findAll('.select-item').forEach((d)=>{
+      d.classList.remove('selected');
+    })
+    divs[idx].classList.add('selected')
+    divs[idx].scrollIntoViewIfNeeded()
+    console.log(divs[idx].classList)
+    currentIndex = idx;
+  }
+
+  inp.addEventListener('keydown', function(event) {
+    let selectableDivs = htmx.findAll('.select-item');
+    // console.log(selectableDivs)
+    switch(event.key) {
+    case 'ArrowDown':
+      event.preventDefault();
+      event.stopPropagation();
+      if (currentIndex === -1) {
+        selectDiv(0, selectableDivs);
+      } else {
+        selectDiv((currentIndex + 1) % selectableDivs.length, selectableDivs);
+      }
+      break;
+    case 'ArrowUp':
+      event.preventDefault();
+      event.stopPropagation();
+      if (currentIndex === -1) {
+        selectDiv(selectableDivs.length - 1);
+      } else {
+        selectDiv((currentIndex - 1 + selectableDivs.length) % selectableDivs.length, selectableDivs);
+      }
+      break;
+    case 'Enter':
+      if (currentIndex !== -1) {
+        event.preventDefault();
+        event.stopPropagation();
+        selectableDivs[currentIndex].click();
+      }
+      break;
+    case 'Escape':
+      resetSelection();
+      currentIndex = -1;
+      break;
+    default:
+      htmx.trigger(inp, 'keyupup')
+    }
   });
 }
 
-function selectItem(ev) {
-  console.log(ev.target, 'select')
-}
--- src/mysystem/ui/select.clj
+++ src/mysystem/ui/select.clj
@@ -15,14 +15,13 @@
 }
 
 .popup {
- // display: none;
   position: absolute;
   top: 100%;
   left: 0;
 }
 
-.select input:focus + .popup {
-  display: block;
+.select-item.selected {
+  background-color: oklch(0.97 0.014 254.604);
 }
 "])
 
@@ -36,16 +35,22 @@
         items (pg/execute! context {:dsql {:select :*
                                            :from (keyword tbl)
                                            :where (when (and q (not (str/blank? q)))
-                                                    [:ilike (keyword col) [:pg/param (str "%" q "%")]])}})]
+                                                    [:ilike (keyword col) [:pg/param (str "%" q "%")]])
+                                           :order-by (keyword col)
+                                           :limit 25}})]
     [:div {:class "divide-y"}
      (when (empty? items) [:div {:class "text-center text-gray-500"} "No items"])
      (for [it items]
-       [:div {:class "px-2 cursor-pointer hover:bg-blue-100"
-              :onclick "
+       [:div.select-item
+        {:class "px-2 cursor-pointer hover:bg-blue-100"
+         :onclick "
 var valel = htmx.find(htmx.closest(event.target, '.select'),'.value');
 valel.innerHTML = '';
-valel.appendChild(event.target.cloneNode(true));
+var clonel = htmx.closest(event.target,'.select-item');
+valel.appendChild(clonel.firstChild.cloneNode(true));
 var sel = htmx.find('#select');
+htmx.find('#popup').innerHTML = ''
+htmx.find('#select').value = ''
 htmx.addClass(sel, 'hidden');
 htmx.removeClass(valel, 'hidden');
 "}  (fmt it)])]))
@@ -55,32 +60,41 @@ htmx.removeClass(valel, 'hidden');
     (let [m (meta v)]
       (str (:ns m) "/" (:name m)))))
 
+(defn chevron [& [size]]
+  [:svg {:class (or size "size-4") :xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox "0 0 24 24" :stroke-width "1.5" :stroke "currentColor"} [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "m19.5 8.25-7.5 7.5-7.5-7.5"}]])
+
 (defn select [context {tbl :table col :column :as opts}]
-  [:div.select {:class "border rounded w-60"}
-   [:div#value.value.hidden
-    {:class "px-2 py-1 hover:bg-gray-100"
-     :onclick "
+  [:div.select {:class "border rounded min-w-80  hover:bg-gray-100"}
+   [:div {:class "w-full flex space-x-1 items-center px-2"}
+    [:div#value.value.hidden
+     {:class "flex-1 px-2 py-1"
+      :onclick "
 var sel = htmx.find('#select');
 var val = htmx.find('#value');
 htmx.addClass(val, 'hidden')
 htmx.removeClass(sel, 'hidden');
 sel.focus()
 "}
-    ]
-   [:input#select
-    {:class "w-full px-2 py-1"
-     :autocomplete "off"
-     :hx-get (h/rpc #'select-search (update opts :html var-name))
-     :name "search"
-     :hx-target "#popup"
-     :hx-trigger "keyup change delay:300"
-     :placeholder "serch.."}]
-   [:div#popup {:class "hidden w-full min-h-40 max-h-60 overflow-y-auto shadow-md border border-gray-100"}]
+     ]
+    [:input#select
+     {:class "flex-1 px-2 py-2 focus:outline-none bg-transparent"
+      :form ""
+      :autocomplete "off"
+      :hx-get (h/rpc #'select-search (update opts :html var-name))
+      :name "search"
+      :hx-target "#popup"
+      :hx-trigger "open, keyupup change delay:300"
+      :placeholder "search.."}]
+    (chevron ["size-6"])]
+   [:div#popup.popup {:class "hidden z-100 w-full p-1 min-h-40 max-h-60 overflow-y-auto shadow-md border border-gray-100 bg-white"}]
    (h/js "inputWithPopup('#select','#popup')")])
 
 
 (defn table-item [tbl]
-  [:div {:class ""}
+  [:div {:class "flex py-1"}
+   [:input {:type "hidden" :name "table_name" :value (:table_name tbl)}]
+   [:span {:class "text-gray-400"} (:table_schema tbl)]
+   [:span {:class "text-gray-300"} "."]
    [:span (:table_name tbl)]])
 
 (defn column-item [tbl]
@@ -89,18 +103,24 @@ sel.focus()
    [:span {:class "text-gray-100"} (:table_name tbl)]
    [:span (:column_name tbl)]])
 
+(defn form-result [context request opts]
+  [:pre {:class "mt-4 bg-gray-100 rounded p-6"} (pr-str (slurp (:body request)))])
+
 (defn index-html [context request]
   (let [today (java.time.LocalDate/now)]
     (h/layout
      context request
      {:content
       [:div
-       ;; select-style
-       (select context {:table "information_schema.tables" :column "table_name" :html #'table-item})
+       [:form {:hx-post (h/rpc #'form-result) :hx-target "#form-result"}
+        select-style
+        (select context {:table "information_schema.tables" :column "table_name" :html #'table-item})
 
-       ;; (select context {:table "information_schema.columns" :column "column_name" :html #'column-item})
-
-       ]})))
+        [:br]
+        ;; (select context {:table "information_schema.columns" :column "column_name" :html #'column-item})
+        (h/btn {:type "submit"} "Submit")
+        ]
+       [:div#form-result]]})))
 
 
 (defn mount-routes [context]
